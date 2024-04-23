689eb21251c78444fabdd47b85622c919b2867e6
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-23 15:22:19 -0500

    Feature: instalacion de next auth, clg de session, status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ButtonAuth from "@/components/ButtonAuth";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ButtonAuth />
     </div>
   );
 };
