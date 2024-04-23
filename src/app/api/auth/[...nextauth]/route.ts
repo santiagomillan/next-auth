a37d211da735d80e1264eae2958047170a2de57d
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-23 15:41:04 -0500

    Feature:  estado global, usuario quemado

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,23 +9,33 @@ const handler = NextAuth({
         email: { label: "email", type: "email", placeholder: "test@test.com" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-            headers: { "Content-Type": "application/json" },
-          }
-        );
-        const user = await res.json();
+      async authorize(credentials, req) {
+        const user = {
+          email: "danny@yopmail.com",
+          name: "John Doe",
+          id: "1",
+        };
+        // const res = await fetch(
+        //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
+        //   {
+        //     method: "POST",
+        //     body: JSON.stringify({
+        //       email: credentials?.email,
+        //       password: credentials?.password,
+        //     }),
+        //     headers: { "Content-Type": "application/json" },
+        //   }
+        // );
+        // const user = await res.json();
 
-        if (user.error) throw user;
+        // if (user.error) throw user;
 
-        return user;
+        // return user;
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
       },
     }),
   ],
